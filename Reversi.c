0940f19f6631ca76936cca5f1350f0b2d543adad
zperkowski <ZdzislawPerkowski@outlook.com>
2016-11-23 23:42:03 +0100

    Navigation in menu

--- Reversi.c
+++ Reversi.c
@@ -1,6 +1,26 @@
 #include "menu.h"
 
+char *player1_name = "Player 1";
+char *player2_name = "Player 2";
+
 int main(int argc, char const *argv[]) {
-  drawMenu();
-  return 0;
+  while (1) {
+    switch (menu()) {
+      case 0: // Play
+
+        break;
+      case 1: // Player 1
+
+        break;
+      case 2: // Player 2
+
+        break;
+      case 3: // Map size
+      
+        break;
+      case 4: // Exit
+        return 0;
+        break;
+    }
+  }
 }
--- menu.h
+++ menu.h
@@ -4,6 +4,20 @@
 
 struct windowSize windowSize;
 
+char *menu_play_unmarked = "[ ] Play";
+char *menu_play_marked = "[X] Play";
+char *menu_player1_unmarked = "[ ] Player 1: ";
+char *menu_player1_marked = "[X] Player 1: ";
+char *menu_player2_unmarked = "[ ] Player 2: ";
+char *menu_player2_marked = "[X] Player 2: ";
+char *menu_mapSize_unmarked = "[ ] Map size: ";
+char *menu_mapSize_marked = "[X] Map size: ";
+char *menu_exit_unmarked = "[ ] Exit";
+char *menu_exit_marked = "[X] Exit";
+
+int option_marked = 0;
+int option_choosen = 0;
+
 void printCentered(char *text) {
   windowSize = getWindowSize(); // Updates the window size
   if (windowSize.width > strlen(text)) {
@@ -21,6 +35,67 @@ void printCentered(char *text) {
 }
 
 void drawMenu() {
-  // Title
-  printCentered("Reversi");
+    // Title
+    printCentered("Reversi\n");
+    switch (option_marked) {
+      case 0:
+        printCentered(menu_play_marked);
+        printCentered(menu_player1_unmarked);
+        printCentered(menu_player2_unmarked);
+        printCentered(menu_mapSize_unmarked);
+        printCentered(menu_exit_unmarked);
+        break;
+      case 1:
+        printCentered(menu_play_unmarked);
+        printCentered(menu_player1_marked);
+        printCentered(menu_player2_unmarked);
+        printCentered(menu_mapSize_unmarked);
+        printCentered(menu_exit_unmarked);
+        break;
+      case 2:
+        printCentered(menu_play_unmarked);
+        printCentered(menu_player1_unmarked);
+        printCentered(menu_player2_marked);
+        printCentered(menu_mapSize_unmarked);
+        printCentered(menu_exit_unmarked);
+        break;
+      case 3:
+        printCentered(menu_play_unmarked);
+        printCentered(menu_player1_unmarked);
+        printCentered(menu_player2_unmarked);
+        printCentered(menu_mapSize_marked);
+        printCentered(menu_exit_unmarked);
+        break;
+      case 4:
+        printCentered(menu_play_unmarked);
+        printCentered(menu_player1_unmarked);
+        printCentered(menu_player2_unmarked);
+        printCentered(menu_mapSize_unmarked);
+        printCentered(menu_exit_marked);
+        break;
+      default:
+        option_marked = 0;
+        break;
+    }
+}
+
+int menu() {
+  option_choosen = 0;
+  while (option_choosen == 0) {
+    drawMenu();
+    switch (getch()) {
+      case 72:
+        if (option_marked > 0)
+          option_marked--;
+        break;
+      case 80:
+        if (option_marked < 4)
+          option_marked++;
+        break;
+      case '\r':
+        option_choosen = 1;
+        break;
+    }
+  }
+  return option_marked;
 }
